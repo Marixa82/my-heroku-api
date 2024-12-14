e85d444588ba820da842073a93193dd847d15503
Maryna Aksakova <121823496+Marixa82@users.noreply.github.com>
2024-12-14 21:36:53 +0200

    create middleware authValidation

---
+++ middlewares/authValidation.js
@@ -0,0 +1,28 @@
+import { HttpError } from '../helpers/index.js';
+import { User } from '../models/user-model.js';
+import jwt from 'jsonwebtoken'
+import 'dotenv/config';
+
+const { JWT_SECRET } = process.env;
+
+const authValidation = async (req, res, next) => {
+    const { authorization = "" } = req.headers;
+    const [bearer, token] = authorization.split(" ");
+
+    if (bearer !== "Bearer") {
+        next(HttpError(401, "Not authorized"))
+    };
+
+    try {
+        const { id } = jwt.verify(token, JWT_SECRET);
+        const user = await User.findById(id);
+        if (!user || !user.token || user.token !== token) {
+            next(HttpError(401, "Not authorized"))
+        }
+        req.user = user;
+        next();
+    } catch {
+        next(HttpError(401, "Not authorized"))
+    }
+}
+export default authValidation;
