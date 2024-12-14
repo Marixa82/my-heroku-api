2da45ac5ef269240545bf59962cd90f1a097f83f
Maryna Aksakova <121823496+Marixa82@users.noreply.github.com>
2024-12-14 21:38:05 +0200

    create auth-route

---
+++ routes/auth-router.js
@@ -0,0 +1,24 @@
+import express from "express";
+import { validateBody, ctrlWrapper } from "../helpers/index.js";
+import { registerSchema} from "../models/user-model.js";
+import { loginController, registerController, logoutController } from "../controllers/auth-controller.js";
+import authValidation from "../middlewares/authValidation.js";
+export const authRouter = express.Router();
+
+// authRouter.get("/verify/:verificationCode", ctrlWrapper(verifyEmailController));
+// authRouter.post(
+//   "/verify",
+//   validateBody(emailSchema),
+//   ctrlWrapper(resendVerifyEmailController)
+// );
+authRouter.post(
+  "/register",
+  validateBody(registerSchema),
+  ctrlWrapper(registerController)
+);
+authRouter.post(
+  "/login",
+  validateBody(registerSchema),
+  ctrlWrapper(loginController)
+);
+authRouter.post("/logout", authValidation, ctrlWrapper(logoutController));
