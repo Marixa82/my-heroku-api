f2379f1fd513a24f90a1e82d36320f2ae3385b5b
Maryna Aksakova <121823496+Marixa82@users.noreply.github.com>
2024-12-14 21:35:23 +0200

    upd auth-controller

--- controllers/auth-controller.js
+++ controllers/auth-controller.js
@@ -1,3 +1,14 @@
+import { User } from "../models/user-model.js";
+import gravatar from "gravatar";
+import bcryptjs from "bcryptjs";
+import { HttpError } from "../helpers/index.js";
+
+const {
+  JWT_SECRET,
+  BASE_URL,
+  FRONTEND_URL,
+} = process.env;
+
 export const registerController = async (req, res) => {
     const { password, email } = req.body;
     const user = await User.findOne({ email });
@@ -58,4 +69,36 @@ export const registerController = async (req, res) => {
     const { id } = req.user;
     await User.findByIdAndUpdate(id, { token: null });
     res.status(204).json();
+  };
+  export const verifyEmailController = async (req, res) => {
+    const { verificationCode } = req.params;
+    const user = await User.findOne({ verificationCode });
+    if (!user) {
+      throw HttpError(401, "Email not found");
+    }
+    await User.findByIdAndUpdate(user._id, {
+      verify: true,
+      verificationCode: "",
+    });
+    res.status(200).redirect(`${FRONTEND_URL}/singin`);
+  };
+  
+  export const resendVerifyEmailController = async (req, res) => {
+    const { email } = req.body;
+    const user = await User.findOne({ email });
+    if (!user) {
+      throw HttpError(401, "Email not found");
+    }
+    if (user.verify) {
+      throw HttpError(401, "Email already verify");
+    }
+    const verifyEmail = {
+      to: email,
+      subject: "Verify email",
+      html: emailVerify(user.verificationCode, BASE_URL),
+    };
+    await sendEmail(verifyEmail);
+    res.json({
+      message: "Verify email send success",
+    });
   };
