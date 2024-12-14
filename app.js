556bc941648eaf8a0353230d1ae4462243b7f44a
Maryna Aksakova <121823496+Marixa82@users.noreply.github.com>
2024-12-14 21:29:59 +0200

    upd app

--- app.js
+++ app.js
@@ -2,17 +2,21 @@ import express from "express";
 import morgan from "morgan";
 import cors from "cors";
 import "dotenv/config";
+import swaggerJSDoc from "swagger-jsdoc";
+import swaggerUi from "swagger-ui-express"; 
+import { authRouter } from "./routes/auth-router.js";
+
 const app = express();
 
 app.use(morgan("tiny"));
 app.use(cors());
 app.use(express.json());
 app.use(express.static("public"));
-// app.use("/api/auth", authRouter);
+app.use("/api/auth", authRouter);
 // app.use("/api/waters", waterRouter);
 // app.use("/api/user", userRouter);
 
-// app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument))
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerJSDoc))
 
 app.use((_, res) => {
   res.status(404).json({ message: "Route not found" });
