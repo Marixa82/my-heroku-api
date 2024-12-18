d4ed6581306c4f7dc32fa94169d6161ae674804d
Maryna Aksakova <121823496+Marixa82@users.noreply.github.com>
2024-12-18 21:30:50 +0200

    create index middlewares

---
+++ middlewares/index.js
@@ -0,0 +1,4 @@
+export { default as authValidation} from './authValidation.js';
+export { isValidId} from './isValidId.js';
+export {isEmptyBody} from './isEmptyBody.js';
+export { upload} from './upload.js';
