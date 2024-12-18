bffd342325be031cb39f653be248cf7937db65da
Maryna Aksakova <121823496+Marixa82@users.noreply.github.com>
2024-12-18 21:30:12 +0200

    upd validateBody

--- helpers/validateBody.js
+++ helpers/validateBody.js
@@ -1,6 +1,6 @@
 import {HttpError} from "../helpers/index.js";
 
-const validateBody = schema => {
+export const validateBody = schema => {
     const func = (req, res, next) => {
         const { error } = schema.validate(req.body);
         if (error) {
@@ -12,4 +12,3 @@ const validateBody = schema => {
     return func;
 }
 
-export default validateBody;
