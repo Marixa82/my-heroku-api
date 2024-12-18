0c9ecb1036b17f3e5c1f866e52298487e6340d77
Maryna Aksakova <121823496+Marixa82@users.noreply.github.com>
2024-12-18 21:31:07 +0200

    create isEmptyBody

---
+++ middlewares/isEmptyBody.js
@@ -0,0 +1,10 @@
+import { HttpError } from "../helpers/index.js";
+
+export const isEmptyBody = (req, res, next) => {
+    if (!Object.keys(req.body).length) {
+        return next(HttpError(400, "missing field favorite"));
+    }
+    next();
+}
+
+// export default isEmptyBody;
